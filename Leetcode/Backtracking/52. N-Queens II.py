82aa49eb72a04c698c0d8d4f7f0fc4cf43aa5001
john-antony <j.antony3001@gmail.com>
2023-08-11 01:16:30 -0400

    added 52

---
+++ Leetcode/Backtracking/52. N-Queens II.py
@@ -0,0 +1,33 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+
+        cols = set()
+        posDiag = set()
+        negDiag = set()
+
+        res = 0
+
+        def backtrack(r):
+            if r == n:
+                nonlocal res
+                res += 1
+                return
+
+            for c in range(n):
+                if c in cols or (r + c) in posDiag or (r - c) in negDiag:
+                    continue
+            
+                cols.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+
+                backtrack(r + 1)
+
+                cols.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
+        
+        backtrack(0)
+
+        return res
+
